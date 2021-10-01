724546f0641e8962a2aeb0c9da2b2ec5ef825958
Morten Berg <58311172+MortenTryti@users.noreply.github.com>
2021-10-01 19:01:04 +0200

    Plotting data and such

---
+++ Project 1/plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+from imageio import imread
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+# Load the terrain
+terrain1 = imread("SRTM_data_Norway_1.tif")
+# Show the terrain
+print(type(terrain1))
+print(terrain1.shape)
+plt.figure()
+plt.title("Terrain over Norway 1")
+plt.imshow(terrain1, cmap="gray")
+plt.xlabel("’X’")
+plt.colorbar()
+plt.ylabel("’Y’")
+plt.show()
